6e51c6e244b7d4965992a39271e2362f066812aa
Kevin Lewis <siweljk@gmail.com>
2023-08-05 19:24:37 -0400

    hi kevin from the pi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import RPIO
 import time
 
 def a_key_callback(gpio_id, val):
-    print("gpio %s: %s" % (gpio_id, val))
+    print("gpio: ", gpio_id, "value: ", val)
 
 # GPIO interrupt callbacks
 RPIO.add_interrupt_callback(7, a_key_callback)
---
+++ notes/README.md
@@ -0,0 +1,1 @@
+Files from https://theremin.music.uiowa.edu/MISpiano.html
