60b815d07f17283b156849786963ee2f977ef8a8
Kevin Lewis <siweljk@gmail.com>
2023-08-05 21:14:27 -0400

    playsound worked time for pi i thinks

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #import the GPIO and time package
 import RPIO
 import time
+import playsound
 
 def a_key_callback(gpio_id, val):
     print("gpio: ", gpio_id, "value: ", val)
+    playsound.playsound('prep_pianoC4.wav')
 
 # GPIO interrupt callbacks
 RPIO.add_interrupt_callback(7, a_key_callback)
---
+++ play1.py
@@ -0,0 +1,3 @@
+import playsound
+playsound.playsound('prep_pianoC4.wav')
+# playsound.playsound('notes/Piano.pp.A4.aiff')
