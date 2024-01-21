61ea17ff16687fe0fb06703e618fd578d8f796f8
Kevin <siweljk@gmail.com>
2024-01-21 11:34:40 -0500

    Update playing for multiple notes when held

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
+#!/usr/bin/env python3
 #import the GPIO and time package
-import RPIO
+
+#using red button
+import RPi.GPIO as GPIO
 import time
 import playsound
+from threading import Thread, Lock
 
-def a_key_callback(gpio_id, val):
-    print("gpio: ", gpio_id, "value: ", val)
-    playsound.playsound('prep_pianoC4.wav')
+second_from_bottom_inside = 2
+class piano_key():
+    def __init__(self, gpio_number):
+        print("start init")
+        self.lockc4 = Lock()
+        self.x = Thread(target=self.play_note)
+        self.x.start()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_number, GPIO.IN)
+        GPIO.add_event_detect(gpio_number, GPIO.BOTH, callback=self.a_key_callback, bouncetime=5)
+        print("done init")
+        
+    def a_key_callback(self, channel):
+        if GPIO.input(second_from_bottom_inside):
+            print("GPIO released")
+            if self.lockc4.locked():
+                self.lockc4.release()
+            
+        else:
+            print("gpio pressed")
+            self.lockc4.acquire(False)
 
-# GPIO interrupt callbacks
-RPIO.add_interrupt_callback(7, a_key_callback)
+    def play_note(self):
+        while (1):
+            if self.lockc4.locked():
+                playsound.playsound('prep_pianoC4.wav')
+        
+def main():
+    # GPIO interrupt callbacks
+    first_key = piano_key(second_from_bottom_inside)
+    
+    while(1):
+        time.sleep(2)
 
-while(1):
-    time.sleep(2)
+if __name__ == '__main__':
+    main()
