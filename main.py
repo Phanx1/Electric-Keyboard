30072598546764954d38261a68f518cb7a6fcf40
Kevin <siweljk@gmail.com>
2024-01-21 12:07:36 -0500

    switch to simpleaudio library

--- main.py
+++ main.py
@@ -4,35 +4,29 @@
 #using red button
 import RPi.GPIO as GPIO
 import time
-import playsound
+import simpleaudio as sa
 from threading import Thread, Lock
 
 second_from_bottom_inside = 2
 class piano_key():
     def __init__(self, gpio_number):
         print("start init")
-        self.lockc4 = Lock()
-        self.x = Thread(target=self.play_note)
-        self.x.start()
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(gpio_number, GPIO.IN)
         GPIO.add_event_detect(gpio_number, GPIO.BOTH, callback=self.a_key_callback, bouncetime=5)
+        self.wave_obj = sa.WaveObject.from_wave_file("prep_pianoC4.wav")
+        self.play_obj = 0
         print("done init")
         
     def a_key_callback(self, channel):
         if GPIO.input(second_from_bottom_inside):
             print("GPIO released")
-            if self.lockc4.locked():
-                self.lockc4.release()
+            if self.play_obj.is_playing():
+                self.play_obj.stop()
             
         else:
             print("gpio pressed")
-            self.lockc4.acquire(False)
-
-    def play_note(self):
-        while (1):
-            if self.lockc4.locked():
-                playsound.playsound('prep_pianoC4.wav')
+            self.play_obj = self.wave_obj.play()
         
 def main():
     # GPIO interrupt callbacks
